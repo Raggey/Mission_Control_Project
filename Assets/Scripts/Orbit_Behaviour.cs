f066021321a625af16159fcc64b2c2849de456f6
Craig Robinson <raggy1995@gmail.com>
2019-08-08 02:34:17 +1000

    Adding of orbit functions and including scene into commit

---
+++ Assets/Scripts/Orbit_Behaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit_Behaviour : MonoBehaviour
+{
+    public float xSpread; //Distance we move on X axis
+    public float zSpread;
+    public float yOffset; //Camera offset
+    public Transform centerPoint;
+
+    public float rotSpeed; //Rotation speed
+    public bool rotateClockwise;
+
+    float timer = 0;
+
+
+    // Update is called once per frame
+    void Update(){
+        timer += Time.deltaTime + rotSpeed;
+        Rotate();
+        transform.LookAt(centerPoint);
+    }
+
+
+    void Rotate() {
+        if (rotateClockwise){
+            float x = -Mathf.Cos(timer) * xSpread;
+            float z = Mathf.Sin(timer) * zSpread;
+            Vector3 pos = new Vector3(x, yOffset, z);
+            transform.position = pos + centerPoint.position;
+        }
+        else {
+            float x = Mathf.Cos(timer) * xSpread; 
+            float z = Mathf.Sin(timer) * zSpread;
+            Vector3 pos = new Vector3(x, yOffset, z);
+            transform.position = pos + centerPoint.position;
+        }
+    }
+
+}
+
+
+
+
+
