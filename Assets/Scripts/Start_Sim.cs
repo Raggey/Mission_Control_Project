658689ada59f774d89624a36e997d26ad53420ff
Craig Robinson <raggy1995@gmail.com>
2020-07-19 21:33:32 +1000

    Added Main Menu scene, Button to start Solar System scene

---
+++ Assets/Scripts/Start_Sim.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Start_Sim : MonoBehaviour {
+
+    public Button startSimButton;
+
+    // Start is called before the first frame update
+    void Start() {
+        Button btn = startSimButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick() {
+        Debug.Log("BUtton Clicked");
+        StartCoroutine(LoadYourAsyncScene());
+    }
+
+    IEnumerator LoadYourAsyncScene() {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Solar_System");
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone) {
+            yield return null;
+        }
+    }
+
+
+}
