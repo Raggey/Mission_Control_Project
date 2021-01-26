7fdf209282d34727570bc370bd0e6d9722e02f96
Craig Robinson <raggy1995@gmail.com>
2021-01-26 12:20:24 +1100

    added tests

---
+++ Tests/NewTestScript.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class NewTestScript
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void NewTestScriptSimplePasses()
+        {
+            // Use the Assert class to test conditions
+             Assert.IsTrue(1,1);
+        }
+     
+    }
+}
