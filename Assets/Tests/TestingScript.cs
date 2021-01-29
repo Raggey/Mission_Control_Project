302609f260a9f7035313cba8c45f0f40ffda3064
Craig Robinson <raggy1995@gmail.com>
2021-01-29 22:32:05 +1100

    moved DLL and added tests

---
+++ Assets/Tests/TestingScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestingScript
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TestingScriptSimplePasses()
+        {
+            // Use the Assert class to test conditions
+            GameObject newObj = new GameObject("SomeName");
+            newObj.AddComponent<Satellite>();
+
+            Assert.IsTrue(true);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TestingScriptWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+            Assert.IsTrue(true);
+        }
+    }
+}
