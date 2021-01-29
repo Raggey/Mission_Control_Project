9b4068bf9b3bc5524ff411e047789fe648e874b3
Craig Robinson <raggy1995@gmail.com>
2021-01-29 22:15:16 +1100

    adding tests, removing plugins

--- Assets/Tests/NewTestScript.cs
+++ Assets/Tests/NewTestScript.cs
@@ -9,10 +9,17 @@ namespace Tests
     public class NewTestScript
     {
         // A Test behaves as an ordinary method
+
+        public GameObject gameobbject;
+
         [Test]
         public void NewTestScriptSimplePasses()
         {
             // Use the Assert class to test conditions
+            gameobbject = new GameObject("Name");
+            gameobbject.AddComponent<Orbit_Behaviour>();
+            Assert.True(true);
+            Object.Destroy(gameobbject);
         }
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
